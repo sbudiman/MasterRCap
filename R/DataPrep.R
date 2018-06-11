240cc29f808dc71030f92131bbfcde3427a669da
San Budiman <sbudiman@rocketmail.com>
2018-06-10 23:50:06 -0700

    Add man/*.Rd files after running roxygen2::roxygenise Update eq_location_clean Add more test functions in test_functions.R

--- R/DataPrep.R
+++ R/DataPrep.R
@@ -3,7 +3,8 @@
 #' @description \code{eq_location_clean} transforms NOAA data frame LOCATION_NAME
 #' column by trimming the country name when applicable.
 #'
-#' @param data A data frame with raw data obtained from NOAA website (see details)
+#' @param RawMEdf A data frame with raw data obtained from NOAA website (see details).
+#' This defaults to sample data frame delivered in package MajorEarthquakes.
 #'
 #' @return A data frame with cleaned LOCATION_NAME column
 #'
@@ -18,15 +19,16 @@
 #' @examples
 #' \dontrun{readr::read_delim(file=file.path("data","results"), delim='\t') %>%
 #' eq_location_clean()}
-eq_location_clean <- function(){
-    for (locindex in 1:length(MajorEarthquakes$LOCATION_NAME)) {
-      loc_list <- MajorEarthquakes$LOCATION_NAME[locindex] %>% str_split('\\:') %>% unlist() %>% str_trim(side = "both")
+eq_location_clean <- function(RawMEdf = MajorEarthquakes){
+    for (locindex in 1:length(RawMEdf$LOCATION_NAME)) {
+      loc_list <- RawMEdf$LOCATION_NAME[locindex] %>% str_split('\\:') %>% unlist() %>% str_trim(side = "both")
       if (length(loc_list) == 1) {
-        MajorEarthquakes$LOCATION_NAME[locindex] <<- loc_list[1]
+        RawMEdf$LOCATION_NAME[locindex] <- loc_list[1]
       } else if (length(loc_list) > 1) {
-        MajorEarthquakes$LOCATION_NAME[locindex] <<- paste(loc_list[-1],separator = '', collapse='')
+        RawMEdf$LOCATION_NAME[locindex] <- paste(loc_list[-1],separator = '', collapse='')
       }
     }
+    return(RawMEdf)
 }
 
 #' Cleans earthquake data from NOAA
@@ -34,7 +36,8 @@ eq_location_clean <- function(){
 #' @description \code{eq_clean_data} cleans date, latitude and longitude, and location name
 #' from the source NOAA data
 #'
-#' @param data A data frame with raw data obtained from NOAA website (see details)
+#' @param RawMEdf A data frame with raw data obtained from NOAA website (see details).
+#' This defaults to sample data frame delivered in package MajorEarthquakes.
 #'
 #' @return A data frame with cleaned date, latitude, longitude and location
 #' columns
@@ -55,7 +58,7 @@ eq_location_clean <- function(){
 #' @examples
 #' \dontrun{readr::read_delim(file=file.path("data","results"), delim='\t') %>%
 #' eq_clean_data()}
-eq_clean_data <- function(RawMEdf = MajorEarthquakesAD){
+eq_clean_data <- function(RawMEdf = MajorEarthquakes){
   if (!is.data.frame(RawMEdf)) stop("RawMEdf must be a data frame")
   if (!("LOCATION_NAME" %in% colnames(RawMEdf))) stop("RawMEdf data frame must have LOCATION_NAME")
   RawMEdf <- RawMEdf %>%
--- tests/testthat/test_functions.R
+++ tests/testthat/test_functions.R
@@ -6,3 +6,65 @@ MajorEarthquakes <- readr::read_delim(filename, delim = "\t")
 test_that("eq_clean_data returns data frame", {
   expect_is(eq_clean_data(MajorEarthquakes), "data.frame")
 })
+
+test_that("eq_clean_data$DATE is Date type", {
+  expect_is(eq_clean_data(MajorEarthquakes)$DATE, "Date")
+})
+
+test_that("eq_clean_data returns LATITUDE and LONGITUDE as numeric values", {
+  expect_is(eq_clean_data(MajorEarthquakes)$LATITUDE, "numeric")
+  expect_is(eq_clean_data(MajorEarthquakes)$LONGITUDE, "numeric")
+})
+
+test_that("eq_location_clean returns a data frame", {
+  expect_is(eq_location_clean(MajorEarthquakes), "data.frame")
+})
+
+test_that("geom_timeline returns ggplot object", {
+  g <- MajorEarthquakes %>% eq_clean_data() %>%
+    dplyr::filter(COUNTRY %in% c("CHINA", "JAPAN"), YEAR > 2010) %>%
+    ggplot2::ggplot(ggplot2::aes(x = DATE,
+                                 y = COUNTRY,
+                                 color = as.numeric(TOTAL_DEATHS),
+                                 size = as.numeric(EQ_PRIMARY)
+    )) +
+    geom_timeline()
+  expect_is(g, "ggplot")
+})
+
+test_that("geom_timeline_label returns ggplot object", {
+  g <- MajorEarthquakes %>% eq_clean_data() %>%
+    dplyr::filter(COUNTRY %in% c("CHINA", "JAPAN"), YEAR > 2010) %>%
+    ggplot2::ggplot(ggplot2::aes(x = DATE,
+                                 y = COUNTRY,
+                                 color = as.numeric(TOTAL_DEATHS),
+                                 size = as.numeric(EQ_PRIMARY)
+    )) +
+    geom_timeline_label(aes(label = LOCATION_NAME))
+  expect_is(g, "ggplot")
+})
+
+test_that("theme_timeline returns ggplot object", {
+  g <- MajorEarthquakes %>% eq_clean_data() %>%
+    dplyr::filter(COUNTRY %in% c("CHINA", "JAPAN"), YEAR > 2010) %>%
+    ggplot2::ggplot(ggplot2::aes(x = DATE,
+                                 y = COUNTRY,
+                                 color = as.numeric(TOTAL_DEATHS),
+                                 size = as.numeric(EQ_PRIMARY)
+    )) +
+    theme_timeline()
+  expect_is(g, "ggplot")
+})
+
+test_that("eq_map returns leaflet object", {
+  l <- MajorEarthquakes %>%
+    eq_clean_data() %>%
+    dplyr::filter(COUNTRY == "INDONESIA" & YEAR >= 2010) %>%
+    dplyr::mutate(popup_text = eq_create_label(.)) %>%
+    eq_map(annot_col = "popup_text")
+  expect_is(l, "leaflet")
+})
+
+test_that("eq_create_label returns character vector", {
+  expect_is(eq_create_label(MajorEarthquakes), "character")
+})
